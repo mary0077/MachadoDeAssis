9c9873f3c12a3823ebd052981177b6d1d2c98cf5
mary0077 <marianagomes201@hotmail.com>
2024-05-05 18:57:17 -0300

    Historia

---
+++ index.js
@@ -0,0 +1,23 @@
+
+ var area= window.document.getElementById('quadrado')
+
+
+function entrar(){
+
+
+ area.innerText='Entrou'
+ area.style.background='blue'
+
+}
+
+function sair(){
+ area.innerText='saiu'
+ area.style.background='red'
+
+}
+function click(){
+  area.innerText='Clicou'
+  area.style.background='yellow'
+  area.style.color='black'
+
+}
